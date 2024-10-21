632fcc7114626b7df16957d490b29af8703bf563
rodrigototy <56210472+rodrigototy@users.noreply.github.com>
2024-10-21 19:07:10 -0300

    Delete .history directory

--- .history/static/js/script_20241021181046.js
+++
@@ -1,4 +0,0 @@
-function flipCard(card) {
-    const cardInner = card.querySelector('.card-inner');
-    cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
-}

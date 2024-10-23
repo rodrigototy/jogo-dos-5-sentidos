5edf31b76047adabb7681956d680a7479ac83c67
rodrigototy <rodrigototy@yahoo.com.br>
2024-10-23 00:53:18 -0300

    Versão 1.0.5 - Ajust layout score cards

--- static/js/game.js
+++ static/js/game.js
@@ -1,4 +1,7 @@
 function flipCard(card) {
-    const cardInner = card.querySelector('.card-inner');
-    cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+  const cardInner = card.querySelector(".card-inner");
+  cardInner.style.transform =
+    cardInner.style.transform === "rotateY(180deg)"
+      ? "rotateY(0deg)"
+      : "rotateY(180deg)";
 }
--- static/js/trainer.js
+++ static/js/trainer.js
@@ -1,16 +1,16 @@
 // Controle do placar
 function incrementScore(teamId) {
-    let pointsElement = document.getElementById(teamId);
-    let currentPoints = parseInt(pointsElement.innerText);
-    pointsElement.innerText = currentPoints + 10;
+  let pointsElement = document.getElementById(teamId);
+  let currentPoints = parseInt(pointsElement.innerText);
+  pointsElement.innerText = currentPoints + 10 + " pontos";
 }
 
 function decrementScore(teamId) {
-    let pointsElement = document.getElementById(teamId);
-    let currentPoints = parseInt(pointsElement.innerText);
-    if (currentPoints > 0) {
-        pointsElement.innerText = currentPoints - 10;
-    }
+  let pointsElement = document.getElementById(teamId);
+  let currentPoints = parseInt(pointsElement.innerText);
+  if (currentPoints > 0) {
+    pointsElement.innerText = currentPoints - 10 + " pontos";
+  }
 }
 
 // Controle do Timer
@@ -18,36 +18,36 @@ let countdown;
 let timeLeft = 20;
 
 function startTimer() {
-    clearInterval(countdown);
-    timeLeft = 20;
-    document.getElementById('timer').innerText = `${timeLeft}s`;
-    document.getElementById('timer').classList.remove("timer-final");
-    document.getElementById('timer').classList.remove("timer-message");
-    document.getElementById('timer').classList.add("timer-start");
+  clearInterval(countdown);
+  timeLeft = 20;
+  document.getElementById("timer").innerText = `${timeLeft}s`;
+  document.getElementById("timer").classList.remove("timer-final");
+  document.getElementById("timer").classList.remove("timer-message");
+  document.getElementById("timer").classList.add("timer-start");
 
-    countdown = setInterval(function () {
-        timeLeft--;
-        document.getElementById('timer').innerText =  `${timeLeft}s`;;
+  countdown = setInterval(function () {
+    timeLeft--;
+    document.getElementById("timer").innerText = `${timeLeft}s`;
 
-        if (timeLeft <= 0) {
-            clearInterval(countdown);
-            document.getElementById('timer').innerText = "Fale o nome da Fruta";
-            document.getElementById('timer').classList.remove("timer-final");
-            document.getElementById('timer').classList.add("timer-message");
-        }
+    if (timeLeft <= 0) {
+      clearInterval(countdown);
+      document.getElementById("timer").innerText = "Fale o nome da Fruta";
+      document.getElementById("timer").classList.remove("timer-final");
+      document.getElementById("timer").classList.add("timer-message");
+    }
 
-        if (timeLeft <= 10) {
-            document.getElementById('timer').classList.remove("timer-start");
-            document.getElementById('timer').classList.add("timer-final");
-        }
-    }, 1000);
+    if (timeLeft <= 10) {
+      document.getElementById("timer").classList.remove("timer-start");
+      document.getElementById("timer").classList.add("timer-final");
+    }
+  }, 1000);
 }
 
 function resetTimer() {
-    clearInterval(countdown);
-    timeLeft = 20;
-    document.getElementById('timer').innerText =  `${timeLeft}s`;
-    document.getElementById('timer').classList.remove("timer-final");
-    document.getElementById('timer').classList.remove("timer-message");
-    document.getElementById('timer').classList.add("timer-start");
+  clearInterval(countdown);
+  timeLeft = 20;
+  document.getElementById("timer").innerText = `${timeLeft}s`;
+  document.getElementById("timer").classList.remove("timer-final");
+  document.getElementById("timer").classList.remove("timer-message");
+  document.getElementById("timer").classList.add("timer-start");
 }

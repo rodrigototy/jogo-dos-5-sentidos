e78f94f61200ec08f3ca939d3b94b8bfdcd25972
rodrigototy <rodrigototy@yahoo.com.br>
2024-10-23 00:13:37 -0300

    Versão 1.0.4 - Ajust layout page Trainer

--- static/js/trainer.js
+++ static/js/trainer.js
@@ -20,15 +20,25 @@ let timeLeft = 20;
 function startTimer() {
     clearInterval(countdown);
     timeLeft = 20;
-    document.getElementById('timer').innerText = timeLeft;
+    document.getElementById('timer').innerText = `${timeLeft}s`;
+    document.getElementById('timer').classList.remove("timer-final");
+    document.getElementById('timer').classList.remove("timer-message");
+    document.getElementById('timer').classList.add("timer-start");
 
     countdown = setInterval(function () {
         timeLeft--;
-        document.getElementById('timer').innerText = timeLeft;
+        document.getElementById('timer').innerText =  `${timeLeft}s`;;
 
         if (timeLeft <= 0) {
             clearInterval(countdown);
             document.getElementById('timer').innerText = "Fale o nome da Fruta";
+            document.getElementById('timer').classList.remove("timer-final");
+            document.getElementById('timer').classList.add("timer-message");
+        }
+
+        if (timeLeft <= 10) {
+            document.getElementById('timer').classList.remove("timer-start");
+            document.getElementById('timer').classList.add("timer-final");
         }
     }, 1000);
 }
@@ -36,5 +46,8 @@ function startTimer() {
 function resetTimer() {
     clearInterval(countdown);
     timeLeft = 20;
-    document.getElementById('timer').innerText = timeLeft;
+    document.getElementById('timer').innerText =  `${timeLeft}s`;
+    document.getElementById('timer').classList.remove("timer-final");
+    document.getElementById('timer').classList.remove("timer-message");
+    document.getElementById('timer').classList.add("timer-start");
 }
